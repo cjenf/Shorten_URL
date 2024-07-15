3a4e8e2ebedf0a05fcbc13b6927cfd323006f7dd
Cjenf <kimichen2907@gmail.com>
2024-07-15 12:30:51 +0800

    fixed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import hashlib
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-Form_=[]
+Form_={}
 
 def generate_short_code(long_url):
     
@@ -23,14 +23,18 @@ async def read_form(request: Request):
 @app.post("/submit", response_class=HTMLResponse)
 async def handle_form(request: Request, url: str = Form(...)):
     try:
-        Form_.append(url)
         short_code = generate_short_code(url)
         short_url = "https://shorten-url-henna.vercel.app/" + short_code
+        Form_[short_code] = url  # 更新字典，使用 short_code 作為鍵
         return templates.TemplateResponse("result.html", {'request': request, "name": short_url})
     except Exception as e:
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.get("/{short_code}")
-async def redirect_to_long_url():
-    return RedirectResponse(url=Form_[-1])
+async def redirect_to_long_url(short_code:str):
+    if short_code in Form_:
+        long_url = Form_[short_code]
+        return RedirectResponse(url=long_url)
+    else:
+        raise HTTPException(status_code=404, detail="URL not found")
